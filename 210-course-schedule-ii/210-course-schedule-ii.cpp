8030b9b5c2a2f6f44badc3561ab0495acad8bbd3
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-06-25 15:05:56 +0530

    Time: 29 ms (62.75%), Space: 14 MB (62.01%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& visited, vector<int>& result){
+        visited[node]=1;
+        for(int i=0;i<graph[node].size();i++){
+            if(visited[graph[node][i]]==1)return false; 
+            if(visited[graph[node][i]]==0){
+                if(!dfs(graph[node][i],graph,visited,result)) 
+                    return false;
+            }
+        }
+        visited[node]=2;
+        result.push_back(node);
+        return true;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            graph[prerequisites[i][0]].push_back(prerequisites[i][1]); 
+        }
+        vector<int> visited(numCourses,0); 		
+        vector<int> result; 
+        bool flag = true; 
+        for(int i=0;i<numCourses;i++){
+            if(visited[i]==0){
+                if(!dfs(i,graph,visited,result)){
+                    flag = false; 
+                    return vector<int>();
+                }
+            }
+        }         
+        return result;
+    }
+};
