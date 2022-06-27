ae0c03fbcc5753359fb235d84e0b9139a51cd7cb
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-06-27 18:43:20 +0530

    Time: 25 ms (49.01%), Space: 17.1 MB (38.07%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -12,7 +12,7 @@ public:
             }
             else if(intervals[i][1]>=newInterval[0] or intervals[i][0] <= newInterval[1]){
                 newInterval[0]=min(intervals[i][0], newInterval[0]);
-                newInterval[1]=max(newInterval[1], intervals[i][1]);
+                newInterval[1]=max(newInterval[1], intervals[i][1]); //merging the overlapping interval
             }
             
         }
