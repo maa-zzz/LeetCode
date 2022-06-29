518f1c4d4474ab6655015b33797cf1a788661fa1
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-06-29 22:49:18 +0530

    Time: 107 ms (40.62%), Space: 18.1 MB (38.04%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -6,8 +6,8 @@ public:
     int m=matrix.size();
     int n=matrix[0].size();
     vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-    for(int i=1;i<=m;++i){
-        for(int j=1;j<=n;++j){
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
             if(matrix[i-1][j-1]=='1'){
                 dp[i][j]=min(min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1])+1;
                 size=max(size,dp[i][j]);
