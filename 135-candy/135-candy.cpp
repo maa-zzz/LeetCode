b86e8fbc0e785f22b9e4190e5a98f46a05a0f12b
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-01 21:28:24 +0530

    Time: 35 ms (45.59%), Space: 17.8 MB (65.61%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -5,18 +5,15 @@ public:
             return 0;
         }
         int n = ratings.size();
-        vector<int> ans;
-        for(int i=0; i<n; i++){
-            ans.push_back(1);
-        }
+        vector<int> ans(n,1);
         for(int i=1;i<n; i++){
             if(ratings[i]>ratings[i-1]){
-                ans[i]=ans[i-1]+1;
+                ans[i]=ans[i-1]+1; 
             }
         }
         for(int i=n-2;i>=0; i--){
             if(ratings[i]>ratings[i+1]){
-                    ans[i]=max(ans[i],ans[i+1]+1);
+                    ans[i]=max(ans[i],ans[i+1]+1); //go both direction
             }
         }
         
