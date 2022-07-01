20d728617df397ca8d386d531163a5a59fbdfe07
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-01 14:50:19 +0530

    Time: 142 ms (30.49%), Space: 69.4 MB (89.45%) - LeetHub

---
+++ 134-gas-station/134-gas-station.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int ans = -1;
+        if(accumulate(gas.begin(), gas.end(),0)<accumulate(cost.begin(),cost.end(),0)){
+           return ans;
+       }
+        else{
+            ans = 0;//definetly works
+        }
+        vector<int>price;
+ //you know there is a solution
+                                      // after you reach end of the array with positive price
+        int total = 0;                          //no need to iterate again as that index is the answer
+        for(int i=0; i<gas.size(); i++){                    //think, its easy
+            total +=(gas[i]-cost[i]);
+            if(total<0){
+                total = 0;
+                ans=i+1;
+            }
+        }
+        return ans;
+    }
+};
