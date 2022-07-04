7e2b7f4a3fb11150e1fd84656c15f50fae99c595
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-04 23:44:27 +0530

    Time: 66 ms (29.28%), Space: 32 MB (55.05%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     	int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
-		int mins = customers.size();
+		int n = customers.size();
 		int directly_satisfied = 0;
 
-		for(int i = 0; i < mins; i++) {
+		for(int i=0; i<n; i++) {
 			if(!grumpy[i]) {
 				directly_satisfied += customers[i];
 				customers[i] = 0;
 			}
 		}
 
-		int secretly_satisfied = 0, sum = 0;
-		for(int i = 0, j = 0; j < mins; j++) {
+		int secretly_satisfied=0, sum = 0;
+		for(int i=0, j=0; j<n; j++) {
 			sum += customers[j];
 			if(j - i == X)
 				sum -= customers[i++];
