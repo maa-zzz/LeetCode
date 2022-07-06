6149b8b92daf16b3ca0f514e335a23eaf2ce9f06
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-06 21:22:51 +0530

    Time: 4 ms (80.76%), Space: 7.9 MB (99.41%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -7,9 +7,9 @@ public:
         int ans = 0;
         int curr = 0;
         for(int i=n-1; i>=0; i--){
-            curr+=temp+satisfaction[i];
-            temp+=satisfaction[i];
-            ans = max(ans, curr);
+            curr+=temp+satisfaction[i]; //adds factors
+            temp+=satisfaction[i];      //repetative addition
+            ans = max(ans, curr);       //katai khooobsurat
         }
         return ans;
     }
