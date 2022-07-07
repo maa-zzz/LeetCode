fc1be9aa8f29e5491696bd8ab48bed14426ff978
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-07 15:20:18 +0530

    Time: 22 ms (58.45%), Space: 10.6 MB (93.77%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -4,7 +4,7 @@ public:
     void dfs(vector<vector<int>>& g, vector<vector<int>>& res, vector<int>&path, int cur){
         path.push_back(cur);
         if(cur==g.size()-1){
-            res.push_back(path);
+            res.push_back(path); //simple dfs backtracking
         }
         else{
             for(auto it: g[cur]){
