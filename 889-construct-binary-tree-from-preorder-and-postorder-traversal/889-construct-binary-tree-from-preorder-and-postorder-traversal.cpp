fdc61139144516d768411a0c593781e2612b4a0f
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-12 21:12:57 +0530

    Time: 22 ms (50.94%), Space: 25.3 MB (92.14%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -9,24 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define null nullptr
 class Solution {
 public:
-    unordered_map<int, int> m; // value->index
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        int len = post.size();
-        for (int i = 0; i < len; i++) m[post[i]] = i;
-        return construct(pre, post, 0, len - 1, 0, len - 1);
+    int i = 0;
+    TreeNode* help(vector<int>& pre, vector<int>& post,int start,int end){
+        //BAse Case
+        if(start>end) return null;
+        if(i>=pre.size()) return null;
+        int j =-1;
+        for(int k = start;k<=end;k++)
+            if(pre[i]==post[k]){
+                j=k;
+                break;
+            }
+        if(j==-1) return null;
+        i++;
+        TreeNode* root = new TreeNode(post[j]);
+        TreeNode* l = help(pre,post,start,j-1);
+        root->left=l;
+        TreeNode* r = help(pre,post,start,j-1);
+        root->right=r;
+        return root;
     }
-    
-    TreeNode* construct(vector<int>& pre, vector<int>& post, int a, int b, int c, int d) {
-        TreeNode* n = new TreeNode(pre[a]);
-        if (a == b) return n;
-        int t = pre[a + 1];
-        int idx = m[t];
-        int len = idx - c + 1;
-        n->left = construct(pre, post, a + 1, a + len, c, c + len - 1);
-        if (idx + 1 == d) return n;
-        n->right = construct(pre, post, a + len + 1, b, idx + 1, d - 1);
-        return n;
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        return help(pre,post,0,post.size()-1);
     }
 };
