5f6400c355186d8c0685ef12fb792251bafae802
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-28 13:38:08 +0530

    Time: 175 ms (89.99%), Space: 87.7 MB (49.02%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if(!prices.size())
@@ -14,5 +14,23 @@ public:
             profit2 = max(profit2, prices[i] - buy2);
         }
         return profit2;
+    }
+};
+*/
+class Solution {
+ public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n<=1) return 0;
+        int k = 2; //number of transactions req in the question
+        vector<vector<int>> cache(k+1, vector<int>(n,0));
+        for(int i=1; i<=k; i++){
+            int tempmax = cache[i-1][0]-prices[0];
+            for(int j=1; j<n; j++){
+                cache[i][j] = max(cache[i][j-1], tempmax+prices[j]);
+                tempmax = max(tempmax, cache[i-1][j]-prices[j]);
+            }
+        }
+        return cache[k][n-1];
     }
 };
