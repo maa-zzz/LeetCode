98eac1f184b361b3c85389f16380957efd5fa289
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-28 18:42:46 +0530

    Time: 6 ms (13.80%), Space: 6 MB (73.91%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -9,3 +9,8 @@ public:
         return 3 * integerBreak(n - 3);
     }
 };
+/*For any integer p strictly greater than 4, it has the property such that 3 * (p - 3) > p, which means breaking it into two integers 3 and p - 3 makes the product larger while keeping the sum unchanged. If p - 3 is still greater than 4, we should break it again into 3 and p - 6, giving 3 * 3 * (p - 6), and so on, until we cannot break it (less than or equal to 4) anymore.
+
+For integer 4, breaking it into 2 * 2 or keeping it as 4 does not change its contribution to the product.
+We cannot have more than two 4s, because 2 * 3 * 3 > 4 * 4. We cannot have more than three 2s because 3 * 3 > 2 * 2 * 2.
+*/
