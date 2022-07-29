878859b5a973b4afa5a56b2706b77288aa94e590
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-29 21:08:28 +0530

    Time: 45 ms (86.28%), Space: 27.7 MB (27.86%) - LeetHub

---
+++ 697-degree-of-an-array/697-degree-of-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        int ans = 0;
+        int maxFrequency = INT_MIN;
+        unordered_map<int,vector<int>> m;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]].push_back(i);
+        }
+        for(auto& p : m) {
+           int frequency = p.second.size();
+           int len = p.second.back()-p.second.front()+1;
+           if (frequency > maxFrequency) {
+               maxFrequency = frequency;
+               ans = len;
+           }
+           else if(frequency == maxFrequency) {
+               ans = min(ans,len);
+           }
+        }
+        return ans;
+    }
+};
