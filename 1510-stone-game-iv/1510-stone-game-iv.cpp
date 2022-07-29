44c00e54a71d42e91d8959d01a9e3b2ed354492c
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-29 14:18:09 +0530

    Time: 286 ms (24.44%), Space: 7.9 MB (52.60%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool winnerSquareGame(int n) {
         vector<int>dp(n+1,0);
-        dp[1] = 1;
         for(int i=0; i<=n; i++){
             for(int k=1; k*k<=i; k++){
-                if(dp[i-k*k]==0) dp[i] = 1;
+                if(!dp[i-k*k]) dp[i] = 1;
             }
         }
         return dp[n];
