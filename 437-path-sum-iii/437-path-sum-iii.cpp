4fb58eb9e635340d5276d0346af1037f9f4d859f
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-31 15:20:07 +0530

    Time: 57 ms (22.80%), Space: 16 MB (76.27%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -10,7 +10,7 @@ public:
         return ans;
     }
     void dfs(TreeNode* root, long sum){
-        if(!root)return;
+        if(!root)return; //dont use root->val>sum for base as target can be negative
         if(root->val==sum)ans+=1;
         dfs(root->left,sum-root->val);
         dfs(root->right,sum-root->val);
