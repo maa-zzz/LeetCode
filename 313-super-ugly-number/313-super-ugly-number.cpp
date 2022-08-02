46fb321fa05ba7d4ec68a9c9654cf8562f16659a
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-02 13:31:45 +0530

    Time: 311 ms (31.81%), Space: 85.9 MB (14.67%) - LeetHub

---
+++ 313-super-ugly-number/313-super-ugly-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int nthSuperUglyNumber(int n, vector<int>& primes) {
+    priority_queue<long long, vector<long long>, std::greater<long long>> pq;
+    pq.push(1);
+    
+    int i = 0;
+    long long curr = 1;
+    while (i < n) {
+      curr = pq.top();
+      pq.pop();
+      ++i;
+      for (int x : primes)
+        pq.push(x * curr);
+      
+      while (!pq.empty() && pq.top() == curr)
+        pq.pop();
+    }
+    
+    return curr;
+  }
+};
