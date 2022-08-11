7ab716009da394d0b76d538250b81a1dcc12597a
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-11 19:44:30 +0530

    Time: 148 ms (50.62%), Space: 41.4 MB (78.29%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n= nums.size();
-        int sum=0, count=0, i=0, j=0;
-        unordered_map<int, int>s;
+        int n = nums.size();
+        int sum = 0, ans = 0;
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            if(sum==k) ans+=1;
+            if(mp.find(sum-k)!=mp.end()) ans+=mp[sum-k]; //what if array is 0000000
+            mp[sum]+=1;
+        }
+        return ans;
         
-        for(i=0; i<n; i++){
-            sum += nums[i];
-            if(sum==k)
-                count++;
-            if(s.find(sum-k)!=s.end())
-                count+=s[sum-k]; //doesn't work with count++ {[0,0,0,0,0,0,0,0,0,0] 0}
-            s[sum]++;
-        } 
+//         int n= nums.size();
+//         int sum=0, count=0, i=0, j=0;
+//         unordered_map<int, int>s;
         
-        return count;
+//         for(i=0; i<n; i++){
+//             sum += nums[i];
+//             if(sum==k)
+//                 count++;
+//             if(s.find(sum-k)!=s.end())
+//                 count+=s[sum-k]; //doesn't work with count++ {[0,0,0,0,0,0,0,0,0,0] 0}
+//             s[sum]++;
+//         } 
+        
+//         return count;
     }
 };
