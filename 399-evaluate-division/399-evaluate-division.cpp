f340944e0b98d0f593ec3c8fbccd70886f45cd47
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-11 21:47:23 +0530

    Time: 3 ms (72.29%), Space: 8.7 MB (12.69%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    void dfs(string s, string d, map<string,vector<pair<string,double>>>&m, set<string> v, double& ans, double temp){
+        if(v.find(s)!=v.end()) return;
+        v.insert(s);
+        if(s==d){
+            ans = temp;
+            return;
+        }
+        else{
+            for(auto a:m[s]){
+                dfs(a.first, d,m, v, ans, temp*a.second);
+            }
+        }
+    }
+    
+    vector<double> calcEquation(vector<vector<string>>& e,vector<double>& v,vector<vector<string>>& q) {
+        map<string,vector<pair<string,double>>> m;
+        for(int i=0; i<e.size(); i++){
+            m[e[i][0]].push_back({e[i][1],v[i]});
+            m[e[i][1]].push_back({e[i][0],1/v[i]});//very important
+            
+        }
+        vector<double>res;
+        for(int i=0; i<q.size(); i++){
+            string s=q[i][0];
+            string d=q[i][1]; //source destination
+            set<string>visited;
+            double ans=-1;
+            if(m.find(s)!=m.end())
+                dfs(s,d,m,visited,ans,1.0);
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
