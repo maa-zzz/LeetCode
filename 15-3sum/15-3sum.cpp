17f9676baddac52b659bcacda8112fdeddfee727
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-11 19:02:24 +0530

    Time: 80 ms (94.39%), Space: 20 MB (77.68%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,33 +7,31 @@ public:
             return ans;
         }
         for(int i=0; i<nums.size()-2; i++){
-            if(i==0 or i>0 and nums[i]!=nums[i-1]){
+            if(i==0 or (i>0 and nums[i]!=nums[i-1])){
                 int start = i+1;
                 int end = nums.size()-1;
-                int sum = 0-nums[i];
+                int sum = -nums[i];
                 while(start<end){
                     if(nums[start]+nums[end]==sum){
-                    ans.push_back({nums[i],nums[start], nums[end]});
-    
-                    while(start<end and nums[start]==nums[start+1]){
-                        start +=1;
+                        ans.push_back({nums[i], nums[start], nums[end]});
+                        while(start<end and nums[start]==nums[start+1]){
+                            start+=1;
+                        } 
+                        while(start<end and nums[end]==nums[end-1]){
+                            end-=1;
+                        }
+                        start+=1;
+                        end-=1;
                     }
-                    while(start<end and nums[end]== nums[end-1]){
-                        end -=1;
+                    else if(nums[start]+nums[end]<sum){
+                        start+=1;
                     }
-                    start+=1;
-                    end-=1;
+                    else{
+                        end-=1;
                     }
-
-                else if(nums[start]+nums[end]<sum){
-                    start +=1;
                 }
-                else{
-                    end-=1;
-                }
-            }
             }
         }
-    return ans;
-}
+        return ans;
+    }
 };
