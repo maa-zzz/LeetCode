2270396ad07985877764dd9d12d6509d7e682422
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-16 15:00:51 +0530

    Time: 531 ms (47.97%), Space: 90.2 MB (73.68%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int x, int y, int &res) {
         int m = grid1.size(), n = grid1[0].size();
-        if(x < 0 || y < 0 || x >= m || y >= n) return;
-        if(grid1[x][y] == 0 && grid2[x][y] == 1) {
+        if(x < 0 || y < 0 || x >= m || y >= n || grid2[x][y] == 0) return;
+        if(!(grid1[x][y] and grid2[x][y] == 1)) {
             res = false;
         }
-        if(grid2[x][y] == 0) return;
         grid2[x][y] = 0;
         dfs(grid1, grid2, x + 1, y, res);
         dfs(grid1, grid2, x, y + 1, res);
@@ -18,7 +17,7 @@ public:
         int ans = 0;
         for(int i = 0; i < m ; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1) {
+                if(grid2[i][j]) {
                     int res = true;
                     dfs(grid1, grid2, i, j, res);
                     ans += res;
