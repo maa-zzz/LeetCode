8237212cbf602953a82d60431d9c6c29c09e4d95
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-16 11:52:32 +0530

    Time: 87 ms (22.64%), Space: 28.1 MB (15.57%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        //this is the second faster solution without hashmap
-        vector<int>arr(26,0);
-        for(int i=0; i<s.size(); i++){
-            arr[s[i]-'a']++;
+        unordered_map<char,vector<int>> hash;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            hash[s[i]].push_back(i);
         }
-        for (int i=0; i <s.size(); i++) {
-            if (arr[s[i]-'a'] == 1) {
-                return i;
-            }
+        int ans = INT_MAX;
+        for(auto it: hash){
+            if(it.second.size()==1) ans = min(ans, it.second[0]);
         }
-        return -1;
+        return ans==INT_MAX ? -1 : ans;
     }
 };
