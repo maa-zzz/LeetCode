863983d17a3d4bab9072d2afc5b9ec579495855f
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-23 19:54:09 +0530

    Time: 56 ms (68.75%), Space: 32.6 MB (42.76%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,20 @@
+
+class FindElements {
+    unordered_set<int> set;
+public:
+    void recover(TreeNode* root, int x) {
+        if (!root) return;
+        root->val = x;
+        set.insert(x);
+        recover(root->left, 2 * x + 1);
+        recover(root->right, 2 * x + 2);
+    }
+    
+    FindElements(TreeNode* root) {
+        recover(root, 0);
+    }
+    
+    bool find(int target) {
+        return set.count(target);
+    }
+};
