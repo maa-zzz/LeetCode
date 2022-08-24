fbd7a94f92837e7eab7ae9a82ce4b0df40de3acb
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-24 20:47:33 +0530

    Time: 256 ms (21.05%), Space: 13.4 MB (60.25%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,37 +1,48 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    struct compare {
-        bool operator()(const ListNode* l, const ListNode* r) {
-            return l->val > r->val;
-        }
-    };
-    ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
-        priority_queue<ListNode *, vector<ListNode *>, compare> q;
-        for(auto l : lists) {
-            if(l)  q.push(l);
-        }
-        if(q.empty())  return NULL;
-
-        ListNode* result = q.top();
-        q.pop();
-        if(result->next) q.push(result->next);
-        ListNode* tail = result;            
-        while(!q.empty()) {
-            tail->next = q.top();
-            q.pop();
-            tail = tail->next;
-            if(tail->next) q.push(tail->next);
-        }
-        return result;
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
     }
 };
+// class Solution {
+// public:
+//     struct compare {
+//         bool operator()(const ListNode* l, const ListNode* r) {
+//             return l->val > r->val;
+//         }
+//     };
+//     ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
+//         priority_queue<ListNode *, vector<ListNode *>, compare> q;
+//         for(auto l : lists) {
+//             if(l)  q.push(l);
+//         }
+//         if(q.empty())  return NULL;
+
+//         ListNode* result = q.top();
+//         q.pop();
+//         if(result->next) q.push(result->next);
+//         ListNode* tail = result;            
+//         while(!q.empty()) {
+//             tail->next = q.top();
+//             q.pop();
+//             tail = tail->next;
+//             if(tail->next) q.push(tail->next);
+//         }
+//         return result;
+//     }
+// };
