d22039da7e39d782645b9b0cd84600f424c6f4b9
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-24 18:23:50 +0530

    Time: 142 ms (57.79%), Space: 42.2 MB (30.31%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -4,7 +4,6 @@ public:
         string ans = "";
         int length = 0;
             for(int i=0; i<s.size(); i++){
-                //odd length
                 int l=i, r=i;
                 while(l>=0 and r<=s.size() and s[l]==s[r]){
                     if((r-l+1)>length){
@@ -18,7 +17,6 @@ public:
                     l-=1;
                     r+=1;
                 }
-                //even length
                 l=i, r=i+1;
                while(l>=0 and r<=s.size() and s[l]==s[r]){
                     if((r-l+1)>length){
