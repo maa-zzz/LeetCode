4b19f719326ca560979a8d96f837427396cb3a07
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-25 12:46:05 +0530

    Time: 19 ms (33.51%), Space: 14.4 MB (15.12%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
-    {
-        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
-        {
-            return;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+         vector<vector<int>>visited(image.size(),vector<int>(image[0].size(),0));
+        
+        queue<pair<int,int>>pq;
+        pq.push({sr,sc});
+        visited[sr][sc]=1;
+        
+        while(pq.size()>0){
+            int i=pq.front().first;
+            int j=pq.front().second;
+            pq.pop();
+            if(i-1>=0&&image[i-1][j]==image[i][j]&& visited[i-1][j]==0){
+                visited[i][j]=1;
+                pq.push({i-1,j});
+            }
+            if(j-1>=0&&image[i][j-1]==image[i][j]&& visited[i][j-1]==0){
+                visited[i][j-1]=1;
+                pq.push({i,j-1});
+            }
+            if(i+1<image.size()&&image[i+1][j]==image[i][j]&& visited[i+1][j]==0){
+                visited[i+1][j]=1;
+                pq.push({i+1,j});
+            }
+            if(j+1<image[0].size()&&image[i][j+1]==image[i][j]&& visited[i][j+1]==0){
+                visited[i][j+1]=1;
+                pq.push({i,j+1});
+            }
+            image[i][j]=newColor;
         }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,val,newColor);
-        dfs(image,i+1,j,val,newColor);
-        dfs(image,i,j-1,val,newColor);
-        dfs(image,i,j+1,val,newColor);
-    }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
-    {
-        int val = image[sr][sc];
-        dfs(image,sr,sc,val,newColor);
         return image;
     }
 };
