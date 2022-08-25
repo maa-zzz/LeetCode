43890f2d34b5ab48e333fa4aedffb484e9b0e88f
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-25 12:56:21 +0530

    Time: 102 ms (8.90%), Space: 18.1 MB (16.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]=='0'){
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
-        
-    }
-    
-    
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j]=='1'){
-                    ans+=1;
-                    dfs(grid, i,j);
+        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> todo;
+                    todo.push({i, j});
+                    while (!todo.empty()) {
+                        pair<int, int> p = todo.front();
+                        todo.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                                grid[r][c] = '0';
+                                todo.push({r, c});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return ans;
+        return islands;
     }
 };
