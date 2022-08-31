1bca24bf27fd5c07375836c481afad3bf39ee05f
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-08-31 12:29:22 +0530

    Time: 23 ms (54.64%), Space: 10.9 MB (22.50%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         //track capacity at all points
-        map<int,int> mp;
+        map<int,int> mp;//oredered coz we approach from smallest time
         for(auto a:trips){
             mp[a[1]]+=a[0];
             mp[a[2]]-=a[0];
