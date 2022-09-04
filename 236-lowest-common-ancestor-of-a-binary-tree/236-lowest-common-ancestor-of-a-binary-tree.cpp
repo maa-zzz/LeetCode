8842d793af426260d5c4adee7b5d9c94a886ef58
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-04 15:36:50 +0530

    Time: 39 ms (11.25%), Space: 14.3 MB (23.13%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -20,3 +20,11 @@ public:
         return left == NULL ? right : left;
     }
 };
+// # If looking for me, return myself
+//         # else look in left and right child
+//         # if both children returned a node, means
+//         # both p and q found so parent is LCA
+//         # either one of the chidren returned a node, meaning either p or q found on left or right branch.
+//         # Example: assuming 'p' found in left child, right child returned 'None'. This means 'q' is
+//         # somewhere below node where 'p' was found we dont need to search all the way, 
+//         # because in such scenarios, node where 'p' found is LCA
