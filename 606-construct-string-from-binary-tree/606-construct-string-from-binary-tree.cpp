a8e0b3d33dcd4f683d2fd1c45e9c96a12193e51a
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-07 12:01:54 +0530

    Time: 35 ms (55.39%), Space: 53.7 MB (30.71%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -11,41 +11,12 @@
  */
 class Solution {
 public:
-    void find_the_string(TreeNode* term, TreeNode* root, string& str){
-    if(term==NULL){
-        str=str+")";
-        return;
-    }
-    
-    int num=term->val;
-    string c=to_string(num);
-    str=str+c+"(";
-    
-    find_the_string(term->left, root, str);
-    
-    if(term->left==NULL && term->right==NULL){
-        str.pop_back();
-        str.pop_back();
-    }
-    
-    str=str+"(";
-    
-    find_the_string(term->right, root, str);
-    
-    if(term->right==NULL){
-        str.pop_back();
-        str.pop_back();
-    }
-    
-    if(term!=root){
-        str=str+")";
-    }
-    
-}
-
-    string tree2str(TreeNode* root) {
-        string str;
-        find_the_string(root, root, str);
-        return str;
+    string tree2str(TreeNode* t) {
+        if (t == NULL) return "";
+        string s = to_string(t->val);
+        if (t->left) s += '(' + tree2str(t->left) + ')';
+        else if (t->right) s += "()";
+        if (t->right) s += '(' + tree2str(t->right) + ')';
+        return s;
     }
 };
