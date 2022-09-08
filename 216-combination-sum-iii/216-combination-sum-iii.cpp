38e02b61981c090808b50c2a0669eb6d146c7b0d
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-08 12:26:20 +0530

    Time: 3 ms (51.87%), Space: 6.4 MB (61.85%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>& v, vector<int>& temp, vector<vector<int>>& ans, int idx, int target, int k) {
-        if (target == 0 and temp.size() == k) {
+    
+    vector<vector<int>> ans;
+    
+    void btrack(vector<int>& temp, int k , int n, int index){
+        if(temp.size()==k and !n){
             ans.push_back(temp);
-            return;
         }
-        
-        for (int i = idx; i < v.size(); i++) {
-            if (i > idx and v[i] == v[i - 1]) continue;
-            if (v[i] > target) break;
-            temp.push_back(v[i]);
-            solve(v, temp, ans, i+1, target - v[i], k);
+        for(int i=index; i<=9; i++){
+            temp.push_back(i);
+            btrack(temp, k, n-i, i+1);
             temp.pop_back();
         }
-        
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> v;
-        for (int i = 1; i <= 9; i++) v.push_back(i);
-        vector<vector<int>> ans;
-        vector<int> ds;
-        solve(v, ds, ans, 0, n, k);
+        vector<int> temp;
+        btrack(temp, k, n, 1);
         return ans;
     }
 };
