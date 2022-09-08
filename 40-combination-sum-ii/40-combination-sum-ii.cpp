b614eddaf74c2c93174b90cf0827453509bb3fc3
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-08 12:21:34 +0530

    Time: 7 ms (76.86%), Space: 10.8 MB (37.13%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     
-    void combination(vector<int>& candidates, int target, vector<int> currcomb, int currindex, vector<vector<int>>& ans){
-        if(target==0){
-            ans.push_back(currcomb);
-            return;
+    vector<vector<int>> ans;
+    
+    void btrack(vector<int>& nums, vector<int>& ccombo, int target, int index){
+        if(!target){
+            ans.push_back(ccombo);
         }
-        else{
-            for(int i=currindex; i<candidates.size(); i++){
-                if(candidates[i]>target){
-                    return;
-                }
-                if(i and candidates[i]==candidates[i-1] and i>currindex){
-                    continue;  //for no duplicatesss(initialilly sorted hence)
-                }
-                currcomb.push_back(candidates[i]);
-                combination(candidates, target-candidates[i], currcomb,i+1, ans);
-                currcomb.pop_back();
-            }          
+        for(int i=index; i<nums.size(); i++){
+            if(nums[i]>target){
+                return;
+            }
+            if(i==index or nums[i]!=nums[i-1]){
+                ccombo.push_back(nums[i]);
+                btrack(nums, ccombo, target-nums[i],i+1);
+                ccombo.pop_back();
+            }
+            
         }
     }
     
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-
-        vector<vector<int>>ans;
-        vector<int> currcomb;
         sort(candidates.begin(), candidates.end());
-        combination(candidates, target, currcomb,0, ans);
+        vector<int> temp;
+        btrack(candidates, temp,target, 0);
         return ans;
     }
 };
