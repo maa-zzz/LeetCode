aa1cee6f3ae44b8e7e78fd3f6f5aa4712f643046
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-10 12:42:43 +0530

    Time: 158 ms (9.01%), Space: 11.8 MB (60.37%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,3 +1,30 @@
+// // Recursion
+// // Time Complexity - O(2^N)
+// // Space Complexity - O(N)
+// class Solution {
+// public:
+    
+//     int solve(vector<int>& prices, int i, int n, int k, bool buy) { // i is index
+//         if(i == n || !k){
+//             return 0;
+//         }
+//         if(!buy){ //can buy or not buy
+//             return max(solve(prices, i+1, n, k, false), solve(prices, i+1, n, k, true)-prices[i]);
+//         }
+//         else{  //can sell or not sell, selling completes transaction
+//             return max(solve(prices,i+1, n, k, true), solve(prices, i+1, n, k-1, false)+prices[i]);
+//         }
+//     }
+//     int maxProfit(int k, vector<int>& prices) {
+//      //1 means buy 0 means sell;
+//         return solve(prices, 0, prices.size(), k, false);
+//     }
+    
+// };
+
+
+// Time complexity - O(n*n*k)
+// Space complexity- O(n*k)
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -9,8 +36,10 @@ public:
             int tempmax = cache[i-1][0]-prices[0];
             for(int j=1; j<n; j++){
                 cache[i][j] = max(cache[i][j-1], tempmax+prices[j]);
+                cout<<cache[i][j]<<" ";
                 tempmax = max(tempmax, cache[i-1][j]-prices[j]);
             }
+            cout<<endl;
         }
         return cache[k][n-1];
     }
