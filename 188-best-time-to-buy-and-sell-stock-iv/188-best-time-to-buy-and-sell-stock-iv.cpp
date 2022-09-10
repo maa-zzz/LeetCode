56d5c2774d0fb53b606b33757a0fac3eea50c3a1
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-10 12:59:21 +0530

    Time: 170 ms (8.97%), Space: 11.8 MB (60.37%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,7 +23,7 @@
 // };
 
 
-// Time complexity - O(n*n*k)
+// Time complexity - O(n*k)
 // Space complexity- O(n*k)
 class Solution {
 public:
@@ -33,11 +33,11 @@ public:
         // int k = 2; //number of transactions req in the question
         vector<vector<int>> cache(k+1, vector<int>(n,0));
         for(int i=1; i<=k; i++){
-            int tempmax = cache[i-1][0]-prices[0];
+            int tempmax = cache[i-1][0]-prices[0];//if bought then
             for(int j=1; j<n; j++){
                 cache[i][j] = max(cache[i][j-1], tempmax+prices[j]);
                 cout<<cache[i][j]<<" ";
-                tempmax = max(tempmax, cache[i-1][j]-prices[j]);
+                tempmax = max(tempmax, cache[i-1][j]-prices[j]);//running profit track
             }
             cout<<endl;
         }
