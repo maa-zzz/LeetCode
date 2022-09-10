91f0b06b48941b3ae68bdeb8abb217012e49d72d
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-09-10 13:14:56 +0530

    Time: 387 ms (35.50%), Space: 51.7 MB (43.34%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -3,10 +3,10 @@ public:
     bool canCross(vector<int>& stones) {
         int n = stones.size();
         unordered_map<int, unordered_set<int>> dp;
-        dp[stones[0]+1] = {1};
+        dp[stones[0]+1] = {1}; //base case
         for(int i=1; i<n; i++){
             int pos = stones[i];
-            for(auto it: dp[pos]){
+            for(auto it: dp[pos]){ //will only iterate if theres already a away to come to it
                 dp[pos+it].insert(it);
                 dp[pos+it+1].insert(it+1); //damn beutiful qn
                 dp[pos+it-1].insert(it-1);
