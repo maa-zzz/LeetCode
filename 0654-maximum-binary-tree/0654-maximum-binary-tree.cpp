049776ded623c50256f3a26d57a851fba5f2bf4d
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-10-19 19:56:30 +0530

    Time: 91 ms (93.09%), Space: 39.4 MB (93.82%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -13,11 +13,9 @@ class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
         vector<TreeNode*> stk;
-        for (int i = 0; i < nums.size(); ++i)
-        {
+        for (int i = 0; i < nums.size(); i++){
             TreeNode* cur = new TreeNode(nums[i]);
-            while (!stk.empty() && stk.back()->val < nums[i])
-            {
+            while (!stk.empty() && stk.back()->val < nums[i]){
                 cur->left = stk.back();
                 stk.pop_back();
             }
